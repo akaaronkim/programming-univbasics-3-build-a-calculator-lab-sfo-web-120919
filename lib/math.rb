717a7da01c44de77093ba2e8a3f828b9db3d88dd
Aaron Kim <akaaronkim@gmail.com>
2019-11-10 19:27:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,6 +1,10 @@
 def addition(num1, num2)
-  sum = num2 + num1
-  puts sum.to_i
+  puts "Enter first value: "
+  num1=gets.chomp.to_i
+  puts "Enter second value: "
+  num2=gets.chomp.to_i
+    sum=num1+num2
+    puts sum
 end
 
 def subtraction(num1, num2)
