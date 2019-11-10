91126a691f0a403735bef47ac00e3c014ac86462
Aaron Kim <akaaronkim@gmail.com>
2019-11-10 19:32:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,30 @@
 def addition(num1, num2)
-  sum = num2.class + num1.class
-  puts sum.class
+  
+  sum = num2 + num1
+  
 end
 
 def subtraction(num1, num2)
+  
   sum = num2 - num1
-  puts sum
+
 end
 
 def division(num1, num2)
   sum = num1 / num2
-  puts sum
+ 
 end
 
 def multiplication(num1, num2)
   sum = num1 * num2
-  puts sum
+  
 end
 
 def modulo(num1, num2)
   sum = num1 % num2
-  puts sum
+  
 end
 
 def square_root(num)
-  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
